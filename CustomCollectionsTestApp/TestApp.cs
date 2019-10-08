548e27ed18f616ce893e20a7fd1df933e8bc20c2
drbodilsson <ludvig.bostrom@gmail.com>
2019-10-08 20:33:14 +0200

    fixade contains

--- CustomCollectionsTestApp/TestApp.cs
+++ CustomCollectionsTestApp/TestApp.cs
@@ -16,8 +16,12 @@ namespace CustomCollectionsTestApp
     /// </summary>
     public partial class TestApp : Form
     {
-        private ObservableList<string> list = new ObservableList<string>();
-         
+        private static ObservableList<string> list = new ObservableList<string>();
+
+        Subscriber subscriber = new Subscriber("sub1", list);
+        TestSubscriber testSubscriber = new TestSubscriber("sub2", list);
+        TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);
+
         public TestApp()
         {
             InitializeComponent();
@@ -25,9 +29,9 @@ namespace CustomCollectionsTestApp
 
         public void Button1_Click(object sender, EventArgs e)
         {
-            Subscriber subscriber = new Subscriber("sub1", list);
+            /*Subscriber subscriber = new Subscriber("sub1", list);
             TestSubscriber testSubscriber = new TestSubscriber("sub2", list);
-            TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);
+            TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);*/
             
             
                 list.Add(textBox1.Text);
@@ -68,14 +72,31 @@ namespace CustomCollectionsTestApp
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Subscriber subscriber = new Subscriber("sub1", list);
+            /*Subscriber subscriber = new Subscriber("sub1", list);
             TestSubscriber testSubscriber = new TestSubscriber("sub2", list);
-            TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);
+            TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);*/
 
             var objToBeRemoved = listBox1.SelectedItem;
             listBox1.Items.Remove(objToBeRemoved);
             list.Remove(objToBeRemoved.ToString());
             System.Windows.Forms.MessageBox.Show(objToBeRemoved.ToString() + " was removed to the list. The list now contains " + subscriber.Count + " items.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*Subscriber subscriber = new Subscriber("sub1", list);
+            TestSubscriber testSubscriber = new TestSubscriber("sub2", list);
+            TestSubscriber2 testSubscriber2 = new TestSubscriber2("sub3", list);*/
+
+            if (list.Contains(textBox1.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("'" + textBox1.Text + "'" + " is in the list");
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("There is no " + "'" + textBox1.Text + "'" + " in the list");
+            }
+
         }
     }
 }
